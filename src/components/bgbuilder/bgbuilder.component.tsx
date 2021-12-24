2eaafa774814979e4608bba9b45d6e34815bafd1
rakeshbade <rakesh.bade@clover.com>
2021-12-24 14:10:10 -0600

    gird scale for mobile

--- src/components/bgbuilder/bgbuilder.component.tsx
+++ src/components/bgbuilder/bgbuilder.component.tsx
@@ -1,22 +1,12 @@
-import { IonButton, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { ColorPaletteModel } from '../../models/color.model'
 import "./bgbuilder.component.style.css";
-import { arrowBack } from 'ionicons/icons';
-const BackgroudBuilderComponent = (props:any) => {
+const BackgroudBuilderComponent = (props:{isPreviewOpen: boolean, platformName: string}) => {
     const selectedColor: ColorPaletteModel = useSelector((state: any) => state.colorReducer);
     const selectedPattern = useSelector((state: any) => state.patternReducer)
     return (
         <div className={'preview-wrapper ' + (props.isPreviewOpen ? props.platformName : '') } >
-            {props.isPreviewOpen ? (<IonHeader >
-                <IonToolbar>
-                    <IonTitle>Preview</IonTitle>
-                    <IonButton slot='start' routerDirection='back' fill="clear" color="medium" onClick={props.close}>
-                        <IonIcon slot="end" icon={arrowBack}  />
-                    </IonButton>
-                </IonToolbar>
-                </IonHeader>) : ''}
             
             <div style={{ color: selectedColor.pattern, backgroundColor: selectedColor.background }} className={`background-item ${selectedPattern.name}`} >
 
--- src/components/preview/preview.component.tsx
+++ src/components/preview/preview.component.tsx
@@ -1,9 +1,10 @@
-import { IonCard, IonModal, IonRippleEffect } from '@ionic/react';
+import { IonButton, IonCard, IonHeader, IonIcon, IonModal, IonRippleEffect, IonTitle, IonToolbar } from '@ionic/react';
 import React, { useEffect, useState } from 'react';
 import BackgroudBuilderComponent from "../bgbuilder/bgbuilder.component"
 import {Device} from '@capacitor/device'
 
 import "./preview.styles.css";
+import { arrowBack } from 'ionicons/icons';
 
 
 const PreviewCompoment = (props: any) => {
@@ -23,7 +24,15 @@ const PreviewCompoment = (props: any) => {
                 <BackgroudBuilderComponent platformName={device} isPreviewOpen={isBuilderOpen}/>
             </IonCard>
             <IonModal isOpen={isBuilderOpen} onDidDismiss={()=>setIsBuilderOpen(false)}>
-                <BackgroudBuilderComponent platformName={device} isPreviewOpen={isBuilderOpen} close={()=>setIsBuilderOpen(false)} />
+            <IonHeader >
+                <IonToolbar>
+                    <IonTitle>Preview</IonTitle>
+                    <IonButton slot='start' routerDirection='back' fill="clear" color="medium" onClick={()=>setIsBuilderOpen(false)}>
+                        <IonIcon slot="end" icon={arrowBack}  />
+                    </IonButton>
+                </IonToolbar>
+                </IonHeader>
+                <BackgroudBuilderComponent platformName={device} isPreviewOpen={isBuilderOpen} />
             </IonModal>
         </div>
     );
